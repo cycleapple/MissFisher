4545b741445d96b92c90c26907defbd4457cf473
CY <cycleapple@gmail.com>
2026-07-29 14:00:27 +0800

    Localize MissFisher 1.6.5.12 interface for zh-TW

--- tools/MissFisherPatcher/Program.cs
+++ tools/MissFisherPatcher/Program.cs
@@ -1,9 +1,9 @@
 using Mono.Cecil;
 using Mono.Cecil.Cil;
 
-if (args.Length != 2)
+if (args.Length != 3)
 {
-    Console.Error.WriteLine("Usage: MissFisherPatcher <input.dll> <output.dll>");
+    Console.Error.WriteLine("Usage: MissFisherPatcher <input.dll> <output.dll> <lang.json>");
     return 2;
 }
 
@@ -66,7 +66,19 @@ ResetBody(
     Instruction.Create(OpCodes.Newobj, licenseConstructor),
     Instruction.Create(OpCodes.Ret));
 
-assembly.Name.Version = new Version(1, 6, 5, 11);
+const string languageResourceName = "MissFisher.Data.Resources.lang.json";
+var languageResource = assembly.MainModule.Resources.SingleOrDefault(x => x.Name == languageResourceName)
+    ?? throw new InvalidOperationException($"Language resource {languageResourceName} was not found.");
+var languageResourceIndex = assembly.MainModule.Resources.IndexOf(languageResource);
+assembly.MainModule.Resources.RemoveAt(languageResourceIndex);
+assembly.MainModule.Resources.Insert(
+    languageResourceIndex,
+    new EmbeddedResource(
+        languageResourceName,
+        languageResource.Attributes,
+        File.ReadAllBytes(args[2])));
+
+assembly.Name.Version = new Version(1, 6, 5, 12);
 assembly.MainModule.Assembly.Name.Version = assembly.Name.Version;
 assembly.Write(args[1]);
 
@@ -78,12 +90,17 @@ var verifiedLicenseMethod = verificationAssembly.MainModule.GetType(licenseProvi
         x.Name == licenseEvaluationMethodName &&
         x.Parameters.Count == 1 &&
         x.Parameters[0].ParameterType.FullName == "System.Byte[]");
-if (verificationAssembly.Name.Version != new Version(1, 6, 5, 11) ||
+var verifiedLanguageResource = verificationAssembly.MainModule.Resources
+    .OfType<EmbeddedResource>()
+    .SingleOrDefault(x => x.Name == languageResourceName);
+if (verificationAssembly.Name.Version != new Version(1, 6, 5, 12) ||
     verifiedExpiryMethod?.Body.Instructions.Count != 1 ||
     verifiedExpiryMethod.Body.Instructions[0].OpCode != OpCodes.Ret ||
     verifiedLicenseMethod?.Body.Instructions.Count != 4 ||
     verifiedLicenseMethod.Body.Instructions[2].OpCode != OpCodes.Newobj ||
-    verifiedLicenseMethod.Body.Instructions[3].OpCode != OpCodes.Ret)
+    verifiedLicenseMethod.Body.Instructions[3].OpCode != OpCodes.Ret ||
+    verifiedLanguageResource is null ||
+    !verifiedLanguageResource.GetResourceData().SequenceEqual(File.ReadAllBytes(args[2])))
 {
     throw new InvalidOperationException("Post-write verification failed.");
 }
@@ -91,5 +108,5 @@ if (verificationAssembly.Name.Version != new Version(1, 6, 5, 11) ||
 Console.WriteLine(
     $"Patched {typeName}.{methodName} and " +
     $"{licenseProviderTypeName}.{licenseEvaluationMethodName}; " +
-    $"version={assembly.Name.Version}; verification=passed");
+    $"replaced {languageResourceName}; version={assembly.Name.Version}; verification=passed");
 return 0;
